832f5809ee29a0962997c3ec10050794dd18f36d
Chengliang_Qian <1096231200@qq.com>
2022-05-19 23:14:11 +0800

    ignored

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "include/swap.h"
+using namespace std;
+
+int main(){
+    int a = 105;
+    int b = 200;
+    swap(a, b);
+
+    return 0;
+
+}
+
---
+++ src/swap.cpp
@@ -0,0 +1,13 @@
+
+#include "../include/swap.h"
+
+
+void swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+
+	cout << "a = " << a << endl;
+	cout << "b = " << b << endl;
+}
