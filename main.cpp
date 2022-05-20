17e719d9d56f6b7f70816e6a2860f558765efe93
Chengliang_Qian <1096231200@qq.com>
2022-05-20 11:51:22 +0800

    202205201151

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 int main(){
     int a = 105;
     int b = 200;
+    int * p;
+    p = &a;
+    *p = 999;
+
     swap(a, b);
+    cout << sizeof(*p) << endl;
+    cout << &a << "\t" << *(&a + 1) << endl;
+    cout << p <<  "\t" << &p <<  "\t" << *(p+1) << endl;
 
     return 0;
 
